9ac4c03f7a7fb3a4f446935a0b004805d92df49b
gary bowen <garybowen@garys-MacBook-Air.local>
2023-02-01 18:48:49 -0500

    why lord

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,7 +3,7 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
-import { useStateContext } from '../context/stateContext.js';
+import { useStateContext } from '../../context/stateContext.js';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
