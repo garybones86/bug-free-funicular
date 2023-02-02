43777fc94f5aa4cf8ceab1349a8cc941f9f2bd7d
gary bowen <garybowen@garys-MacBook-Air.local>
2023-02-02 14:48:14 -0500

    yes  lord

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -8,7 +8,7 @@ const HeroBanner = ({ heroBanner }) => {
     <div className="hero-banner-container">
       <div>
         <p className="beats-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
+        < h3 className="midText">{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
         <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
 
@@ -17,8 +17,8 @@ const HeroBanner = ({ heroBanner }) => {
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
-            <h5>Description</h5>
-            <p>{heroBanner.desc}</p>
+            <h5 className='hero-desc-title'>Description</h5>
+            <p className='hero-des'>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
